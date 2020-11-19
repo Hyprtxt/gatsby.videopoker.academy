e1426da44eee4afba11e5624c1eaf647b4f528ed
Taylor <tdy721@gmail.com>
2020-11-18 22:38:36 -0700

    ok

---
+++ src/pages/game.js
@@ -0,0 +1,13 @@
+import React from "react"
+// import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const GamePage = () => (
+  <Layout>
+    <SEO title="Video Poker" />
+    <p>This is cool</p>
+  </Layout>
+)
+
+export default GamePage
