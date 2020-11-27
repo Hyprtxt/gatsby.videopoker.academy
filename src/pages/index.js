4458128e486ac9f513f9e709b9940c92c793edf3
Taylor <tdy721@gmail.com>
2020-11-26 18:57:48 -0700

    working!

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,18 @@ import React, { useContext } from "react"
 import { store } from "src/store"
 import { Link } from "gatsby"
 import SEO from "src/components/seo"
+import useIsClient from "src/hooks/use-is-client"
 
 const IndexPage = () => {
+  const { isClient, key } = useIsClient()
   const sessionMachine = useContext(store)
   const { state } = sessionMachine
   let user = "people"
+  if (!isClient) return null
   // console.log("HEADER", state)
-  if (state !== undefined) {
-    if (state.value === "active") {
-      user = state.context.user.username
-      // user = state.context.user.Handle
-    }
+  if (state.value === "active") {
+    user = state.context.user.username
+    // user = state.context.user.Handle
   }
   return (
     <>
@@ -21,25 +22,20 @@ const IndexPage = () => {
         <h1>{`Hi ${user}`}</h1>
         <p>This is a website, you may:</p>
         <ul>
-          {state !== undefined
-            ? state.value === "inactive" && <li>P̶l̶a̶y̶ ̶t̶h̶e̶ ̶g̶a̶m̶e̶ ̶o̶n̶l̶i̶n̶e̶</li>
-            : null}
-          {state !== undefined
-            ? state.value === "inactive" && (
-                <li>
-                  <a href="//api.videopoker.academy/connect/google">
-                    Join/Login
-                  </a>
-                </li>
-              )
-            : null}
-          {state !== undefined
-            ? state.value === "active" && (
-                <li>
-                  <Link to="/game">Play this game online</Link>
-                </li>
-              )
-            : null}
+          {state.value === "inactive" && <li>P̶l̶a̶y̶ ̶t̶h̶e̶ ̶g̶a̶m̶e̶ ̶o̶n̶l̶i̶n̶e̶</li>}
+
+          {state.value === "inactive" && (
+            <li>
+              <a href="//api.videopoker.academy/connect/google">Join/Login</a>
+            </li>
+          )}
+
+          {state.value === "active" && (
+            <li>
+              <Link to="/game">Play this game online</Link>
+            </li>
+          )}
+
           <li>
             <Link to="/offline">Play this game on localStorage</Link>
           </li>
@@ -52,13 +48,12 @@ const IndexPage = () => {
           {/* <li>
             <Link to="/page-2">Visit Page 2</Link>
           </li> */}
-          {state !== undefined
-            ? state.value === "active" && (
-                <li>
-                  <Link to="/logout">Logout</Link>
-                </li>
-              )
-            : null}
+
+          {state.value === "active" && (
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          )}
         </ul>
         <div className="clearfix"></div>
       </div>
