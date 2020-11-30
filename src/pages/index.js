6e1ed43dd218a1d92c7425aaac49c79dda9c3b87
Taylor <tdy721@gmail.com>
2020-11-29 21:23:03 -0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import SEO from "src/components/seo"
 import useIsClient from "src/hooks/use-is-client"
 
 const IndexPage = () => {
-  const { isClient, key } = useIsClient()
+  const { isClient } = useIsClient()
   const sessionMachine = useContext(store)
   let { state } = sessionMachine
+  let stateValue = isClient ? state.value : "inactive"
   let user = "people"
   useEffect(() => {
-    if (state.value === "active") {
+    if (stateValue === "active") {
       user = state.context.user.username
       // user = state.context.user.Handle
     }
@@ -22,30 +23,27 @@ const IndexPage = () => {
         <h1>{`Hi ${user}`}</h1>
         <p>This is a website, you may:</p>
         <ul>
-          {state.value === "inactive" && <li>P̶l̶a̶y̶ ̶t̶h̶e̶ ̶g̶a̶m̶e̶ ̶o̶n̶l̶i̶n̶e̶</li>}
-          {state.value === "inactive" && (
+          {stateValue === "inactive" && <li>P̶l̶a̶y̶ ̶t̶h̶e̶ ̶g̶a̶m̶e̶ ̶o̶n̶l̶i̶n̶e̶</li>}
+          {stateValue === "inactive" && (
             <li>
               <Link to="/login">Join/Login with provider</Link>
             </li>
           )}
-          {state.value === "active" && (
+          {stateValue === "active" && (
             <li>
               <Link to="/poker">Play this game online</Link>
             </li>
           )}
-          <li>
-            <Link to="/offline">Play this game on localStorage</Link>
-          </li>
-          <li>
+          {/* <li>
             <a href="//knockout.videopoker.academy">Play JS Video Poker</a>
           </li>
           <li>
             <a href="//jquery.videopoker.academy">Play JQuery Poker Trainer</a>
-          </li>
+          </li> */}
           {/* <li>
             <Link to="/page-2">Visit Page 2</Link>
           </li> */}
-          {state.value === "active" && (
+          {stateValue === "active" && (
             <li>
               <Link to="/logout">Logout</Link>
             </li>
