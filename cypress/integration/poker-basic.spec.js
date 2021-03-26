ba5a30eca24f2782e25d54a6ee1346a792cf64b0
Taylor <taylor@eightfold.io>
2021-03-26 13:34:22 -0700

    more test stuff

--- cypress/integration/poker-basic.spec.js
+++ cypress/integration/poker-basic.spec.js
@@ -1,8 +1,33 @@
 /// <reference types="cypress" />
+
+const removeUser = ( token ) => {
+  cy.request({
+    method: "GET",
+    url: 'http://localhost:1337/remove-my-account',
+    auth: {
+      'bearer': token
+    }
+  }).then(
+    (response) => {
+      // console.log( response.body, "REMOVED A USER?" )
+      expect(response.body).to.have.property('username', 'removeme')
+    }
+  )
+}
+
 let UserToken = ''
+
 context('Window', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8000')
+  })
+
+  it('The API server should be up and OK', () => {
+    cy.request('http://localhost:1337/users-permissions').then(
+      (response) => {
+        expect(response.body).to.have.property('message', 'ok')
+      }
+    )
   })
 
   it('cy.window() - get the global window object', () => {
@@ -20,7 +45,25 @@ context('Window', () => {
     cy.title().should('include', 'Video').should('include', 'Poker').should('include', 'Academy')
   })
 
-  it('remove the test user if found', () => {})
+  it('remove the test user if found', () => {
+    const userLogin = {"identifier":"removeme","password":"thisgoodpass"}
+    cy.request({
+      method: 'POST', 
+      url: 'localhost:1337/auth/local',
+      body: userLogin,
+      failOnStatusCode: false
+    }).then(
+      (response) => {
+        console.log( response.body, response.body.jwt )
+        if( response.body.statusCode !== 400 ) {
+          removeUser(response.body.jwt)
+        } else {
+          // No test user present
+          expect(response).to.have.property('status', 400)
+        }
+      }
+    )
+  })
 
   it('The play button should be inactive', () => {
     cy.get('#link-inactive').should('be.visible')
@@ -57,19 +100,22 @@ context('Window', () => {
     cy.get('#link-play').should('be.visible')
   })
 
-  it('Play a Hand of Classic Video Poker', () => {
-    const WAIT = 200
+  it('Play 5 Hands of Classic Video Poker', () => {
+    const WAIT = 0
+    let i = 0
     cy.restoreLocalStorage()
     cy.get('#link-play a').click()
     cy.wait(WAIT)
     cy.get('#play-classic').click()
-    cy.wait(WAIT)
-    cy.get('#game-button-start').click()
-    cy.wait(WAIT)
-    cy.get('#game-button-score').click()
-    cy.wait(WAIT)
-    cy.get('#status').should('contain', 'score')
-    cy.wait(WAIT)
+    do {
+      cy.get('#game-button-start').click()
+      cy.get('#status').should('contain', 'active')
+      cy.get('#game-button-score').click()
+      cy.get('#status').should('contain', 'score')
+      cy.wait(WAIT)
+      i++;
+    }
+    while (i < 5);
   })
 
   it('User can log out', () => {
@@ -83,17 +129,6 @@ context('Window', () => {
 
   it('remove the new user from strapi', () => {
     cy.restoreLocalStorage()
-    cy.request({
-      method: "GET",
-      url: 'http://localhost:1337/remove-my-account',
-      auth: {
-        'bearer': UserToken
-      }
-    }).then(
-      (response) => {
-        // console.log( response.body, "REMOVED A USER?" )
-        expect(response.body).to.have.property('username', 'removeme')
-      }
-    )
+    removeUser(UserToken)
   })
 })
